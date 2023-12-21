99be551e3e346e58f2149c0357e740e350a4237d
mirjunaid <mirjunaid526@gmail.com>
2023-12-21 10:24:08 +0530

    Third Commit

--- main/my_demo_project.c
+++ main/my_demo_project.c
@@ -5,5 +5,12 @@ void app_main(void)
     printf("Hello, world!");
 
     printf("This is my second line of code!!!!");
+
+    printf("This is my third commit to this repo!!!!");
+
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    
 }
 
