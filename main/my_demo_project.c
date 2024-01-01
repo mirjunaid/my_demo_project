009e1c73c2797c07b6ff13bdcc30eda3641ddf29
mirjunaid <mirjunaid526@gmail.com>
2024-01-01 19:27:57 +0530

    My first led blink program

--- main/my_demo_project.c
+++ main/my_demo_project.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
 
-void app_main(void)
-{
-    printf("Hello, world!");
+void app_main() {
+    gpio_pad_select_gpio(GPIO_NUM_2);
+    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
 
-    printf("This is my second line of code!!!!");
-
-    printf("This is my third commit to this repo!!!!");
-
-    int a = 10;
-    int b = 20;
-    int c = a + b;
-    
+    while (1) {
+        gpio_set_level(GPIO_NUM_2, 1); // Turn on the LED
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1 second
+        gpio_set_level(GPIO_NUM_2, 0); // Turn off the LED
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1 second
+    }
 }
-
